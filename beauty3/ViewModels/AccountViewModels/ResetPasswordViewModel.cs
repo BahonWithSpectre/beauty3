ff42f2f2782691cda5ff770ba82da1a3c22b3e56
bakytzhan.shymkentay <bakytzhan.shymkentay@DESKTOP-RM08NSD>
2020-10-28 20:18:31 +0300

    2810fix

---
+++ beauty3/ViewModels/AccountViewModels/ResetPasswordViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace beauty3.ViewModels.AccountViewModels
+{
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        [Display(Name = "Номер телефона")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [MinLength(6, ErrorMessage = "Құпиясөз 6 символдан кем болмауы тиіс!")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "Құпиясөздер сәйкес емес!")]
+        public string ConfirmPassword { get; set; }
+
+        public string Code { get; set; }
+    }
+}
